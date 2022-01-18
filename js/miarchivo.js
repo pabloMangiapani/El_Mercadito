1f241ec8d5c16979b2989ab086d9d4328e107970
pabloMangiapani <pmangiapani23@gmail.com>
2022-01-18 17:17:51 -0300

    formateo codigo primer desafio

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,18 +1,19 @@
-let edad = ''
-let nombre = ''
-let apellido = ''
+let edad = "";
+let nombre = "";
+let apellido = "";
 // pedido para validar edad
-while(edad < 18  || edad > 17) {
-    edad = prompt('ingresa tu edad');
-        if(edad < '18'){
-        alert('Lo siento no tenes edad para beber'); }  
-        else if(edad > '17'){
-        alert('Genial! Ya podes ingresar tus datos para empezar a comprar tu bebida');
-// si es mayor pide nombre y apellido        
-            nombre = prompt('ingresa tu nombre');
-        if(nombre != ''){
-            apellido = prompt('ingresa tu apellido');
-        }
-        
-    } 
+while (edad < 18 || edad > 17) {
+  edad = prompt("ingresa tu edad");
+  if (edad < "18") {
+    alert("Lo siento no tenes edad para beber");
+  } else if (edad > "17") {
+    alert(
+      "Genial! Ya podes ingresar tus datos para empezar a comprar tu bebida"
+    );
+    // si es mayor pide nombre y apellido
+    nombre = prompt("ingresa tu nombre");
+    if (nombre != "") {
+      apellido = prompt("ingresa tu apellido");
+    }
+  }
 }
