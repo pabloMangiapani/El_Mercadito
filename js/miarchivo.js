10c4605f5bfcd94d7638b8cbc55b48db7b5790df
pabloMangiapani <pmangiapani23@gmail.com>
2022-01-18 17:10:49 -0300

    cambio el algoritmo de repeticion primer desafio

---
+++ js/miarchivo.js
@@ -0,0 +1,18 @@
+let edad = ''
+let nombre = ''
+let apellido = ''
+// pedido para validar edad
+while(edad < 18  || edad > 17) {
+    edad = prompt('ingresa tu edad');
+        if(edad < '18'){
+        alert('Lo siento no tenes edad para beber'); }  
+        else if(edad > '17'){
+        alert('Genial! Ya podes ingresar tus datos para empezar a comprar tu bebida');
+// si es mayor pide nombre y apellido        
+            nombre = prompt('ingresa tu nombre');
+        if(nombre != ''){
+            apellido = prompt('ingresa tu apellido');
+        }
+        
+    } 
+}
